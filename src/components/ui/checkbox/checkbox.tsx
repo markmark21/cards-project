0126caa877f934f7f4dff5d8277a400f6bb22eaf
Mark Khramov <hramov.mark321@gmail.com>
2024-01-09 12:45:48 +0300

    checkbox component

---
+++ src/components/ui/checkbox/checkbox.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react'
+
+import { Check } from '@/assets/icons'
+import { Typography } from '@/components'
+import * as CheckboxRadix from '@radix-ui/react-checkbox'
+import * as LabelRadix from '@radix-ui/react-label'
+import { clsx } from 'clsx'
+
+import s from './checkbox.module.scss'
+
+export type CheckboxProps = {
+  checked?: boolean
+  className?: string
+  disabled?: boolean
+  id?: string
+  label?: string
+  onChange?: (checked: boolean) => void
+  position?: 'left'
+  required?: boolean
+}
+export const Checkbox: FC<CheckboxProps> = ({
+  checked,
+  className,
+  disabled,
+  id,
+  label,
+  onChange,
+  position,
+  required,
+}) => {
+  const classNames = {
+    buttonWrapper: clsx(s.buttonWrapper, disabled && s.disabled, position === 'left' && s.left),
+    container: clsx(s.container, className),
+    indicator: s.indicator,
+    label: clsx(s.label, disabled && s.disabled),
+    root: s.root,
+  }
+
+  return (
+    <div className={classNames.container}>
+      <LabelRadix.Root>
+        <Typography as={'label'} className={classNames.label} variant={'body2'}>
+          <div className={classNames.buttonWrapper}>
+            <CheckboxRadix.Root
+              checked={checked}
+              className={classNames.root}
+              disabled={disabled}
+              id={id}
+              onCheckedChange={onChange}
+              required={required}
+            >
+              {checked && (
+                <CheckboxRadix.Indicator>
+                  <Check />
+                </CheckboxRadix.Indicator>
+              )}
+            </CheckboxRadix.Root>
+          </div>
+          {label}
+        </Typography>
+      </LabelRadix.Root>
+    </div>
+  )
+}
