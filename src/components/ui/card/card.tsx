ade28a7ea51167cbba79f8144439a177bdf71848
Mark Khramov <hramov.mark321@gmail.com>
2024-01-09 12:19:48 +0300

    card component refactor

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,5 +1,15 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import { clsx } from 'clsx'
+
 import s from './card.module.scss'
 
-export const Card = () => {
-  return <div className={s.card}></div>
-}
+export type CardProps = {} & ComponentPropsWithoutRef<'div'>
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(({ className, ...restProps }, ref) => {
+  const classNames = {
+    root: clsx(s.root, className),
+  }
+
+  return <div className={classNames.root} ref={ref} {...restProps}></div>
+})
