6f540a770e0cb9b18b37ae2ac09fe3f8d7ef1c14
Mark Khramov <hramov.mark321@gmail.com>
2024-01-25 13:16:31 +0300

    login form

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,36 +1,36 @@
-import { useController, useForm } from 'react-hook-form'
+import {useController, useForm} from 'react-hook-form'
 
-import { Checkbox } from '@/components'
-import { TextField } from '@/components'
-import { Button } from '@/components'
+import {Checkbox} from '@/components'
+import {TextField} from '@/components'
+import {Button} from '@/components'
 
 type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
+    email: string
+    password: string
+    rememberMe: boolean
 }
 
 export const LoginForm = () => {
-  const { control, handleSubmit, register } = useForm<FormValues>()
+    const {control, handleSubmit, register} = useForm<FormValues>()
 
-  const onSubmit = (data: FormValues) => {
-    console.log(data)
-  }
+    const onSubmit = (data: FormValues) => {
+        console.log(data)
+    }
 
-  const {
-    field: { onChange, value },
-  } = useController({
-    control,
-    defaultValue: false,
-    name: 'rememberMe',
-  })
+    const {
+        field: {value, onChange},
+    } = useController({
+        name: 'rememberMe',
+        control,
+        defaultValue: false,
+    })
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField {...register('email')} label={'email'} />
-      <TextField {...register('password')} label={'password'} />
-      <Checkbox checked={value} label={'remember me'} onCheckedChange={onChange} />
-      <Button type={'submit'}>Submit</Button>
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField {...register('email')} label={'email'}/>
+            <TextField {...register('password')} label={'password'}/>
+            <Checkbox onChange={onChange} checked={value} label={'remember me'}/>
+            <Button type="submit">Submit</Button>
+        </form>
+    )
 }
