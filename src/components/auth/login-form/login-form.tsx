7324d90930e776b532d49b18ca90de693bfb1779
Mark Khramov <hramov.mark321@gmail.com>
2024-01-25 15:25:50 +0300

    email and password validation

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,36 +1,57 @@
-import {useController, useForm} from 'react-hook-form'
+import { useController, useForm } from 'react-hook-form'
 
-import {Checkbox} from '@/components'
-import {TextField} from '@/components'
-import {Button} from '@/components'
+import { Button, Checkbox, TextField } from '@/components'
 
 type FormValues = {
-    email: string
-    password: string
-    rememberMe: boolean
+  email: string
+  password: string
+  rememberMe: boolean
 }
 
+const emailRegex =
+  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+
 export const LoginForm = () => {
-    const {control, handleSubmit, register} = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>()
 
-    const onSubmit = (data: FormValues) => {
-        console.log(data)
-    }
+  console.log(errors)
 
-    const {
-        field: {value, onChange},
-    } = useController({
-        name: 'rememberMe',
-        control,
-        defaultValue: false,
-    })
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <TextField {...register('email')} label={'email'}/>
-            <TextField {...register('password')} label={'password'}/>
-            <Checkbox onChange={onChange} checked={value} label={'remember me'}/>
-            <Button type="submit">Submit</Button>
-        </form>
-    )
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    defaultValue: false,
+    name: 'rememberMe',
+  })
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        {...register('email', {
+          pattern: { message: 'Invalid email', value: emailRegex },
+          required: 'Email is required',
+        })}
+        errorMessage={errors.email?.message}
+        label={'email'}
+      />
+      <TextField
+        {...register('password', {
+          minLength: { message: 'Password should be at least 3 characters long', value: 3 },
+        })}
+        errorMessage={errors.password?.message}
+        label={'password'}
+      />
+      <Checkbox checked={value} label={'remember me'} onValueChange={onChange} />
+      <Button type={'submit'}>Submit</Button>
+    </form>
+  )
 }
