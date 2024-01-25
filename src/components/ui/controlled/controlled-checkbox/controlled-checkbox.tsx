27797308756810e35d0c9d9639bf0c8d86c4b332
Mark Khramov <hramov.mark321@gmail.com>
2024-01-26 01:11:12 +0300

    controlled-checkbox done

--- src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -1,3 +1,18 @@
-export const ControlledCheckbox = () => {
-  return <div></div>
+import { useController } from 'react-hook-form'
+
+import { Checkbox, CheckboxProps } from '@/components'
+
+type Props = {
+  control: any
+  name: string
+} & CheckboxProps
+export const ControlledCheckbox = ({ control, name, ...rest }: Props) => {
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    name,
+  })
+
+  return <Checkbox checked={value} label={name} onValueChange={onChange} {...rest} />
 }
