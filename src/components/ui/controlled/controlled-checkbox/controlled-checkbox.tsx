252a120c25beea3266abe36e0d9a472feb2b7adf
Mark Khramov <hramov.mark321@gmail.com>
2024-01-26 16:17:58 +0300

    new props

--- src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -4,12 +4,24 @@ import { Checkbox, CheckboxProps } from '@/components'
 
 type Props<T extends FieldValues> = UseControllerProps<T> &
   Omit<CheckboxProps, 'checked' | 'onValueChange'>
-export const ControlledCheckbox = <T extends FieldValues>({ control, name, ...rest }: Props<T>) => {
+export const ControlledCheckbox = <T extends FieldValues>({
+  control,
+  defaultValue,
+  disabled,
+  name,
+  rules,
+  shouldUnregister,
+  ...rest
+}: Props<T>) => {
   const {
     field: { onChange, value },
   } = useController({
     control,
+    defaultValue,
+    disabled,
     name,
+    rules,
+    shouldUnregister,
   })
 
   return <Checkbox {...rest} checked={value} label={'remember me'} onValueChange={onChange} />
