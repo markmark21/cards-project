f494392c6383eee375a88756a01f1f88aecfe01a
Mark Khramov <hramov.mark321@gmail.com>
2024-01-31 14:19:29 +0300

    table stories

---
+++ src/components/ui/table/table.stories.tsx
@@ -0,0 +1,224 @@
+import { useMemo, useState } from 'react'
+
+import { Typography } from '@/components'
+import { Meta } from '@storybook/react'
+
+import {
+  Column,
+  Sort,
+  Table,
+  TableBody,
+  TableCell,
+  TableEmpty,
+  TableHead,
+  TableHeadCell,
+  TableHeader,
+  TableRow,
+} from './'
+
+export default {
+  component: Table,
+  title: 'Components/Table',
+} as Meta<typeof Table>
+
+export const Default = {
+  args: {
+    children: (
+      <>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Название</TableHeadCell>
+            <TableHeadCell align={'center'}>Описание</TableHeadCell>
+            <TableHeadCell>Ссылка</TableHeadCell>
+            <TableHeadCell>Тип</TableHeadCell>
+            <TableHeadCell>Вид</TableHeadCell>
+            <TableHeadCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>Web Basic</TableCell>
+            <TableCell>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut sed do eiusmod tempoei usmodr sit amet, consectetur adipiscing elit, sed
+              do...
+            </TableCell>
+            <TableCell>
+              <Typography
+                as={'a'}
+                href={'https://it-incubator.io/'}
+                target={'_blank'}
+                variant={'link1'}
+              >
+                Какая-то ссылка кудато на какой-то источник с информациейо ссылка кудато на какой-то
+                источник
+              </Typography>
+            </TableCell>
+            <TableCell>Основной</TableCell>
+            <TableCell>Читать</TableCell>
+            <TableCell>🦎</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Web Basic</TableCell>
+            <TableCell>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut sed do eiusmod tempoei usmodr sit amet, consectetur adipiscing elit, sed
+              do...
+            </TableCell>
+            <TableCell>
+              Какая-то ссылка кудато на какой-то источник с информациейо ссылка кудато на какой-то
+              источник
+            </TableCell>
+            <TableCell>Основной</TableCell>
+            <TableCell>Читать</TableCell>
+            <TableCell>✨</TableCell>
+          </TableRow>
+        </TableBody>
+      </>
+    ),
+  },
+}
+
+const data = [
+  {
+    category: 'Основной',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    id: '01',
+    link: 'Какая-то ссылка кудато на какой-то источник с информациейо ссылка кудато на какой-то',
+    title: 'Web Basic',
+    type: 'Читать',
+  },
+  {
+    category: 'Основной',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    id: '02',
+    link: 'Какая-то ссылка куда-то',
+    title: 'Web Basic',
+    type: 'Читать',
+  },
+  {
+    category: 'Основной',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    id: '03',
+    link: 'Какая-то ссылка кудато на какой-то источник с информациейо ссылка кудато на какой-то. Какая-то ссылка кудато на какой-то источник с информациейо ссылка куда-то на какой-то',
+    title: 'Web Basic',
+    type: 'Читать',
+  },
+]
+
+export const WithMapMethod = {
+  args: {
+    children: (
+      <>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Название</TableHeadCell>
+            <TableHeadCell align={'center'}>Описание</TableHeadCell>
+            <TableHeadCell>Ссылка</TableHeadCell>
+            <TableHeadCell>Тип</TableHeadCell>
+            <TableHeadCell>Вид</TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(item => (
+            <TableRow key={item.id}>
+              <TableCell>{item.title}</TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell>{item.link}</TableCell>
+              <TableCell>{item.category}</TableCell>
+              <TableCell>{item.type}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </>
+    ),
+  },
+}
+const data2 = [
+  {
+    cardsCount: 10,
+    createdBy: 'John Doe',
+    title: 'Project A',
+    updated: '2023-07-07',
+  },
+  {
+    cardsCount: 5,
+    createdBy: 'Jane Smith',
+    title: 'Project B',
+    updated: '2023-07-06',
+  },
+  {
+    cardsCount: 8,
+    createdBy: 'Alice Johnson',
+    title: 'Project C',
+    updated: '2023-07-05',
+  },
+  {
+    cardsCount: 3,
+    createdBy: 'Bob Anderson',
+    title: 'Project D',
+    updated: '2023-07-07',
+  },
+  {
+    cardsCount: 12,
+    createdBy: 'Emma Davis',
+    title: 'Project E',
+    updated: '2023-07-04',
+  },
+]
+
+const columns: Array<Column> = [
+  {
+    key: 'name',
+    sortable: true,
+    title: 'Name',
+  },
+  {
+    key: 'cardsCount',
+    sortable: true,
+    title: 'Cards',
+  },
+  {
+    key: 'updated',
+    title: 'Last Updated',
+  },
+  {
+    key: 'createdBy',
+    title: 'Created by',
+  },
+]
+
+export const WithSort = {
+  render: () => {
+    const [sort, setSort] = useState<Sort>(null)
+    const sortedString = useMemo(() => {
+      if (!sort) {
+        return null
+      }
+
+      return `${sort.key}-${sort.direction}`
+    }, [sort])
+
+    console.log(sortedString)
+
+    return (
+      <table>
+        <TableHeader columns={columns} onSort={setSort} sort={sort} />
+        <tbody>
+          {data2.map(item => (
+            <tr key={item.title}>
+              <td>{item.title}</td>
+              <td>{item.cardsCount}</td>
+              <td>{item.updated}</td>
+              <td>{item.createdBy}</td>
+              <td>icons...</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  },
+}
+export const Empty = {
+  render: () => <TableEmpty />,
+}
